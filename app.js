9d3dd529315f83c9bc4c0735b6c80c10bd0f7e44
Faraz-Nasir <faraz.nasir46@gmail.com>
2021-02-15 09:21:42 +0530

    Completed

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ const player1_score=document.querySelector("#player1")
 const player2_score=document.querySelector("#player2")
 const drop_down=document.querySelector("#score-max")
 let max_score=0
+
+function alertWin(winner){
+    alert("Player"+winner + "  is the winner.")
+}
 function checkwin(player1_score,player2_score,max_score){
     if(player1_score===max_score){
-        console.log("Player1 Wins")
+        alertWin(1)
         return 1
     }
     else if(player2_score===max_score){
-        console.log("player2 wins")
+        alertWin(2)
         return 2
     }
 }
@@ -24,6 +28,7 @@ function increment(max_score,btn_value){
         return true
     }
 }
+
 drop_down.addEventListener("change",function(){
     //used ("this") here because in a regular function
     //if arrow function then use event(e)
@@ -38,7 +43,10 @@ btn1.addEventListener("click",function(){
         player1_score.textContent++
     }
     if(checkwin(player1_score.textContent,player2_score.textContent,max_score)===1){
-        console.log(1)
+        player1_score.style.color="green"
+        player2_score.style.color="red"
+        btn2.style.background="red"
+        btn1.style.background="green"
         btn1.disabled=true
         btn2.disabled=true
     }
@@ -51,7 +59,11 @@ btn2.addEventListener("click",()=>{
         
     }
     if(checkwin(player1_score.textContent,player2_score.textContent,max_score)===2){
-        console.log(2)
+        
+        player2_score.style.color="green"
+        player1_score.style.color="red"
+        btn2.style.background="green"
+        btn1.style.background="red"
         btn1.disabled=true
         btn2.disabled=true
     }
@@ -63,6 +75,8 @@ resetbtn.addEventListener("click",()=>{
     btn2.disabled=false
     player1_score.textContent=0
     player2_score.textContent=0
+    player2_score.style.color="black"
+    player1_score.style.color="black"
     max_score=0
     drop_down.value="Select One"
 })
